c0b36d1978d796805d91e3305c26a63141070bd9
AstroPilot2808 <ajayohm28@gmail.com>
2024-01-03 19:08:22 -0500

    lambda/anonymous functions

--- students.py
+++ students.py
@@ -1,4 +1,16 @@
+students = []
+
 with open("students.csv") as file:
     for line in file:
-        name, house = line.split(",")
-        print(f"Hi, My name is {name.rstrip()}. And, I live in {house.rstrip()}.")
+        name, house = line.rstrip().split(",")
+        student = {"name":name, "house": house}
+        students.append(student)
+
+def get_name(student):
+    return student["name"]
+
+def get_house(student):
+    return student["house"]
+
+for student in sorted(students, key=lambda student: student["name"]): # This is a lambda funtion. This works without every actually defining the function. 
+    print(f"{student['name']} is in {student['house']}")
